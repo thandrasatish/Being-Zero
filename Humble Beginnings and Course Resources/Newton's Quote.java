3ea3df6fc26c7b6ef89fb3d4566d38959e28aba3
thandrasatish <61319878+thandrasatish@users.noreply.github.com>
2022-01-15 23:50:46 +0530

    Newton's Quote.java

---
+++ Humble Beginnings and Course Resources/Newton's Quote.java
@@ -0,0 +1,7 @@
+class Test
+{
+    public static void main(String args[])
+    {
+      System.out.println("\"If I have seen further than others, it is by standing upon the \'shoulders of giants\'.\"\n- Isaac Newton");
+    }
+}
