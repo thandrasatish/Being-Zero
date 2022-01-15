9947d79372346badb126cf24313ba0e406275e2b
thandrasatish <61319878+thandrasatish@users.noreply.github.com>
2022-01-15 23:57:27 +0530

    A+B Input-Output Practice - (IV).py

---
+++ Programming Fundamentals/A+B Input-Output Practice - (IV).py
@@ -0,0 +1,9 @@
+def main():
+    while True:
+        try:
+            a,b=map(int,input().split())
+            print(a+b)
+        except EOFError:
+            break
+if __name__ == "__main__":
+	main()
