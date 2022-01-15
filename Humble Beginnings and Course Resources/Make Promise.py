60a34d16ae22b80c834e2c7cfc3a569cca96b17d
thandrasatish <61319878+thandrasatish@users.noreply.github.com>
2022-01-15 23:49:03 +0530

    Make Promise.py

---
+++ Humble Beginnings and Course Resources/Make Promise.py
@@ -0,0 +1,5 @@
+def main():
+	print("I promise to 'WORK HARD'; to become \"PROFICIENT\" at Programming!"+"\n"+"1 hour regular practice for 5 days is much effective than doing nothing for 4 days and planning to do 5 hours on last day!")
+
+if __name__ == '__main__':
+	main()
