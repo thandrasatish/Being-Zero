bbb46690348c63747c4b1f8c4c61ac8c85def878
thandrasatish <61319878+thandrasatish@users.noreply.github.com>
2022-01-15 23:54:04 +0530

    BEST CODER.py

---
+++ Humble Beginnings and Course Resources/BEST CODER.py
@@ -0,0 +1,8 @@
+#Write code so that on execution, Program prints following output (Case-Sensitive).
+#This program prohibits to use most common ways to print output - so you may have to do some research on how to print output when common ways are not allowed.
+import sys
+def main():
+	sys.stdout.write("I want to be the BEST CODER!")
+
+if __name__ == '__main__':
+	main()
