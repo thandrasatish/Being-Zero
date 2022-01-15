042c202289791a33d0b8504b73aaa4d64b45d5e1
thandrasatish <61319878+thandrasatish@users.noreply.github.com>
2022-01-15 23:51:45 +0530

    Printing Days of Week.py

---
+++ Humble Beginnings and Course Resources/Printing Days of Week.py
@@ -0,0 +1,11 @@
+def main():
+    print("MONDAY")
+    print("TUESDAY")
+    print("WEDNESDAY")
+    print("THURSDAY")
+    print("FRIDAY")
+    print("SATURDAY")
+    print("SUNDAY")
+
+if __name__ == "__main__":
+    main()
